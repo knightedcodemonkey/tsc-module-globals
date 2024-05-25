041314d2e823f2b37026db72e7782f37a67c92aa
KCM <knightedcodemonkey@gmail.com>
2024-05-25 14:36:02 -0500

    chore: initial commit.

---
+++ README.md
@@ -0,0 +1,7 @@
+The `tsc` compiler seems to report errors differently when using globals from different module systems (es or commonjs).
+
+This example uses `__dirname` and `meta.import.dirname` but only the latter is reported when targeting commonjs via the `"type"` field in package.json. The `__dirname` silenty passes when targeting es modules, but both produce runtime errors in node.
+
+- `npm i`
+- `npm run esm` (note no compile error but the oput causes a runtime error)
+- `npm run cjs` (note there _is_ a compile error)
---
+++ buildCjs.ts
@@ -0,0 +1,19 @@
+import { readFile, writeFile } from "node:fs/promises";
+import { spawnSync } from "node:child_process";
+import pkg from "./package.json";
+
+const src = (await readFile("src/file.ts")).toString();
+
+pkg.type = "commonjs";
+await writeFile("./package.json", JSON.stringify(pkg, null, 2));
+await writeFile("src/file.ts", src.replace(/__dirname/, "import.meta.dirname"));
+spawnSync("tsc", { stdio: "inherit" });
+spawnSync("node", ["./dist/file.js"], { stdio: "inherit" });
+
+// Cleanup
+pkg.type = "module";
+await writeFile("./package.json", JSON.stringify(pkg, null, 2));
+await writeFile(
+  "src/file.ts",
+  src.replace(/\bimport\.meta\.dirname\b/, "__dirname"),
+);
